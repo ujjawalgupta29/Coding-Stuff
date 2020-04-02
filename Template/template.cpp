8211fde7703c887de782cf237b5db6907d34b71e
ujjawalgupta29 <gupta.ujjawal29@gmail.com>
2020-04-02 21:52:04 +0530

    template_updated

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define v(n) vector<int>(n)
-#define for(i) for(int i=0; i<n; i++)
+#define v(n) vector<int> v(n)
+#define ifor(i, a, b) for(int i=a; i<b; i++)
+#define dfor(i, a, b) for(int i=a; i>=b; i--)
+typedef vector<int> VI;
 
 int main()
 {
