6cf54aefc9dc6a204981b93d3e400cd8e6d15dc3
ujjawalgupta29 <gupta.ujjawal29@gmail.com>
2020-05-02 21:33:49 +0530

    optimised

--- LeetCode/balance_bin_tree.cpp
+++ LeetCode/balance_bin_tree.cpp
@@ -1,3 +1,32 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int depth(TreeNode* root)
+//     {
+//         if(!root)
+//             return 0;
+//         return max(depth(root->left), depth(root->right)) + 1;
+//     }
+    
+//     bool isBalanced(TreeNode* root) {
+//         if(root == NULL)
+//             return true;
+        
+//         return isBalanced(root->left) && isBalanced(root->right) && 
+//             abs(depth(root->left) - depth(root->right)) <= 1;
+//     }
+// };
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -11,18 +40,27 @@
  */
 class Solution {
 public:
-    int depth(TreeNode* root)
+    int check(TreeNode* root)
     {
         if(!root)
             return 0;
-        return max(depth(root->left), depth(root->right)) + 1;
+        
+        int left = check(root->left);
+        if(left == -1)
+            return -1;
+        
+        int right = check(root->right);
+        if(right == -1)
+            return -1;
+        
+        if(abs(left - right) > 1)
+            return -1;
+        return max(left, right) + 1;
+        
     }
     
     bool isBalanced(TreeNode* root) {
-        if(root == NULL)
-            return true;
-        
-        return isBalanced(root->left) && isBalanced(root->right) && 
-            abs(depth(root->left) - depth(root->right)) <= 1;
+        int res = check(root);
+        return (res == -1) ? 0 : 1;
     }
 };
